47472772e40d85e120d4537cef3ab9e3d32796a6
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-07-06 10:09:24 -0300

    token.h implemented

---
+++ src/include/token.h
@@ -0,0 +1,26 @@
+#ifndef GAB_TOKEN_H
+#define GAB_TOKEN_H
+typedef struct TOKEN_STRUCT 
+{
+    char* value;
+    enum
+    {
+        TOKEN_ID,
+        TOKEN_EQUALS,
+        TOKEN_LPAREN,
+        TOKEN_NPAREN,
+        TOKEN_RBRACE,
+        TOKEN_LBRACE,   
+        TOKEN_COLON,
+        TOKEN_COMMA,
+        TOKEN_LT,
+        TOKEN_GT,
+        TOKEN_RIGHT_ARROW,
+        TOKEN_INT,
+        TOKEN_SEMI
+    } type;
+} token_T;
+
+token_T* init_token(char* value, int type);
+#endif
+

bfe95bc77c7ef68b35f1041ccc767d1c705b7bfe
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-07-06 10:33:56 -0300

    lexer_advance and lexer_next_token created to header

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -1,6 +1,8 @@
 #ifndef GAB_LEXER_H
 #define GAB_LEXER_H
 
+#include "token.h"
+
 typedef struct LEXER_STRUCT
 {
     char* src;
@@ -9,4 +11,7 @@ typedef struct LEXER_STRUCT
 } lexer_T;
 
 lexer_T* init_lexer(char* src);
+void lexer_advance(lexer_T* lexer);
+token_T* lexer_next_token(lexer_T* lexer);
+
 #endif

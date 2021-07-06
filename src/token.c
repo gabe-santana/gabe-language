60aca25969ddd00c678a78d52aeb3aa22cad9f46
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-07-06 10:14:36 -0300

    token.c implemented

---
+++ src/token.c
@@ -0,0 +1,11 @@
+#include "include/token.h"
+#include <stdlib.h> 
+
+token_T* init_token(char* value, int type)
+{
+    token_T* token = calloc(1, sizeof(struct TOKEN_STRUCT));
+    token->value = value;
+    token->type = type;
+    
+    return token;
+}

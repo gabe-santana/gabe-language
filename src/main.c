dd823839d3a3e8a9bdad27c2409b3db9d663172d
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-07-07 09:21:11 -0300

    main.c added

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "include/gab.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    if(argc < 2)
+    {
+        printf("Unspecified input file");
+        return 1;
+    }
+    return 0;
+}
